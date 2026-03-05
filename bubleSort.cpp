df1ff275c2a72b75b1d17e3304e8e8d3342d4f82
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-05 10:57:19 +0700

    menambahkan input

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -5,3 +5,15 @@ using namespace std;
 int arr[20]; // deklarasi variabel global array a dengan ukuran 20
 int n; // deklarasi variabel global n dengan menyimpan banyak elemen
 
+void input(){ // procedur untuk input
+    while (true){
+        cout << "masukkan banyak elemen pada array :"; //output ke layar
+        cin >> n; // input dari pengguna
+        if (n <=20) // jika n kurang dari atau sama dengan 20
+            break; // keluar dari loop
+        else { // jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n" ;
+        }
+    }
+
+}
