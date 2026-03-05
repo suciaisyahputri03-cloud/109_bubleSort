60daf82aa8ec78bf2dc43db6c8728d1bd2038faa
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-05 11:49:23 +0700

    menambahkan return

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -47,3 +47,33 @@ void bubbleSortArray(){     // procedur utuk mengurutkan array
         cout << endl;
     } while (pass <= n - 1); // step 5
 }
+
+void display(){
+    cout << endl;
+    cout << "========================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "========================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++){
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1){
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
+
+int main() {
+    
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
