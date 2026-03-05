4fdc93affda3247b83879edec0aa858561b5eefd
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-05 11:27:35 +0700

    menambhakan leSortArray

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -15,5 +15,35 @@ void input(){ // procedur untuk input
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n" ;
         }
     }
+cout << endl;
+cout << "===================" << endl;
+cout << "masukkan elemen array" << endl;
+cout << "===================" << endl;
 
+for (int i = 0; i < n; i++){            // looping dengan i dimulai dari 0
+    cout << "data ke-" << (i +i) << ": "; // output ke layar 
+    cin >> arr[i];                          // input dari pengguna
+}
+}
+void bubbleSortArray(){     // procedur utuk mengurutkan array
+    int pass = 1; // step 1
+
+    do{
+        for (int j = 0; j <= n - 1 - pass; ) { // setep 2
+            if (arr[j] > arr[j + 1]){ // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+        }
+        pass = pass + 1; // step 4
+
+        cout << "\npass" << pass - 1 << ": "; // number of pass
+        for (int k = 0; k < n; k++){
+            cout << arr[k] << " ";              // menampilkan data pada number of pass
+        }
+        cout << endl;
+    } while (pass <= n - 1); // step 5
 }
