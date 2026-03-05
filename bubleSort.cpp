616b9a9c3463fafb841f1cfdd214165ca524d162
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-05 10:46:09 +0700

    menambahkan array 20

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int arr[20]; // deklarasi variabel global array a dengan ukuran 20
+int n; // deklarasi variabel global n dengan menyimpan banyak elemen
+
